9498812b7956073b64f63332ad5307e3baf75627
Afshan-Khan-49 <afshan97khan@gmail.com>
2022-10-05 17:52:09 +0530

    Cassandra Db Configuration

---
+++ src/main/java/connection/DataStaxAstraProperties.java
@@ -0,0 +1,24 @@
+package connection;
+
+import java.io.File;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+// datastax astra cassandra connection configuration
+@ConfigurationProperties(prefix = "datastax.astra")
+public class DataStaxAstraProperties {
+
+    private File secureConnectBundle;
+
+    public File getSecureConnectBundle() {
+        return secureConnectBundle;
+    }
+
+    public void setSecureConnectBundle(File secureConnectBundle) {
+        this.secureConnectBundle = secureConnectBundle;
+    }
+
+    
+
+    
+}
